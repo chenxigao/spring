9d0522007ac625abd6fa2129d61116b2b9ea1ece
vagrant <vagrant@homestead>
2019-05-24 16:34:37 +0000

    Seeding topics and users data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,8 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
+class DatabaseSeeder extends Seeder {
     /**
      * Run the database seeds.
      *
@@ -11,7 +10,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
-		$this->call(TopicsTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(TopicsTableSeeder::class);
     }
 }
