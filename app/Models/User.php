9e7a2f90320ab5ecafd02e4af3eb5c1fe4dbb16b
vagrant <vagrant@homestead>
2019-06-01 16:34:52 +0000

    Notify author topic replied

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,10 +4,24 @@ namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Auth;
 
-class User extends Authenticatable
-{
-    use Notifiable;
+class User extends Authenticatable {
+    use Notifiable {
+        notify as protected laravelNotify;
+    }
+
+    public function notify($instance)
+    {
+        //如果要通知的用户是当前用户，就不必通知了
+        if ($this->id == Auth::id()){
+            return;
+        }
+
+        $this->increment('notification_count');
+        $this->laravelNotify($instance);
+        
+    }
 
     /**
      * The attributes that are mass assignable.
@@ -15,7 +29,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'avatar', 'introduction'
+            'name', 'email', 'password', 'avatar', 'introduction',
     ];
 
     /**
@@ -24,7 +38,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+            'password', 'remember_token',
     ];
 
     public function topics()
@@ -45,8 +59,15 @@ class User extends Authenticatable
     {
         //权限代码重构
         return $this->id == $model->user_id;
-        
+
     }
 
+    public function markAsRead()
+    {
+        $this->notification_count = 0;
+        $this->save();
+        $this->unreadNotifications->markAsRead();
+        
+    }
 
 }
