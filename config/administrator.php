b44bf771f7eb63188746629f3f6382f7e117aaa4
vagrant <vagrant@homestead>
2019-06-02 11:14:07 +0000

    Admin can manage roles and permissions

--- config/administrator.php
+++ config/administrator.php
@@ -1,6 +1,6 @@
 <?php
 
-return array(
+return [
 
     /*
      * Package URI
@@ -8,7 +8,7 @@ return array(
      * @type string
      */
     //后台 URI 入口
-    'uri' => 'admin',
+        'uri'                  => 'admin',
 
     /*
      *  Domain for routing.
@@ -16,7 +16,7 @@ return array(
      *  @type string
      */
     //后台专属域名，没有的话可以留空
-    'domain' => '',
+        'domain'               => '',
 
     /*
      * Page title
@@ -24,7 +24,7 @@ return array(
      * @type string
      */
     //应用名称，在页面标题和左上角站点名称处显示
-    'title' => config('APP_NAME', 'Spring'),
+        'title'                => config('APP_NAME', 'Spring'),
 
     /*
      * The path to your model config directory
@@ -32,7 +32,7 @@ return array(
      * @type string
      */
     //模型配置信息文件存放目录
-    'model_config_path' => config_path('administrator'),
+        'model_config_path'    => config_path('administrator'),
 
     /*
      * The path to your settings config directory
@@ -40,7 +40,7 @@ return array(
      * @type string
      */
     //配置信息文件存放目录
-    'settings_config_path' => config_path('administrator/settings'),
+        'settings_config_path' => config_path('administrator/settings'),
 
     /*
      * 后台菜单数组，多维数组渲染结果为多级嵌套菜单。
@@ -65,11 +65,13 @@ return array(
      * 		'Analytics' => array('E-Commerce' => 'page.ecommerce.analytics'),
      *	)
      */
-    'menu' => [
-            '用户与权限' => [
-                    'users',
-            ],
-    ],
+        'menu'                 => [
+                '用户与权限' => [
+                        'users',
+                        'roles',
+                        'permissions',
+                ],
+        ],
 
     /*
      *权限控制的回调函数。
@@ -77,10 +79,10 @@ return array(
      * 此回调函数需要返回 true 或 false ，用来检测当前用户是否有权限访问后台。
      * `true` 为通过，`false` 会将页面重定向到 `login_path` 选项定义的 URL 中。
      */
-    'permission' => function () {
-        //只要能管理内容的用户就允许访问后台
-        return Auth::check() && Auth::user()->can('manage_contents');
-    },
+        'permission'           => function () {
+            //只要能管理内容的用户就允许访问后台
+            return Auth::check() && Auth::user()->can('manage_contents');
+        },
 
     /*
      * 使用布尔值来设定是否使用后台主页面。
@@ -88,39 +90,39 @@ return array(
      * 如值为 `true`，将使用 `dashboard_view` 定义的视图文件渲染页面；
      * 如值为 `false`，将使用 `home_page` 定义的菜单条目来作为后台主页
      */
-    'use_dashboard' => false,
+        'use_dashboard'        => false,
 
     /*
      * 设置后台主页视图文件，由 `use_dashboard` 选项决定
      */
-    'dashboard_view' => '',
+        'dashboard_view'       => '',
 
     /*
      *用来作为后台主页的菜单条目，由 `use_dashboard` 选项决定，菜单指的是 `menu` 选项
      */
-    'home_page' => 'users',
+        'home_page'            => 'users',
 
     /*右上角『返回主站』按钮的链接*/
-    'back_to_site_path' => '/',
+        'back_to_site_path'    => '/',
 
     //当选项 `permission` 权限检测不通过时，会重定向用户到此处设置的路径
-    'login_path' => 'login',
+        'login_path'           => 'login',
 
     /*
      * The logout path is the path where Administrator will send the user when they click the logout link
      *
      * @type string
      */
-    'logout_path' => false,
+        'logout_path'          => false,
 
     //允许在登录成功后使用 Session::get('redirect') 将用户重定向到原本想要访问的后台页面
-    'login_redirect_key' => 'redirect',
+        'login_redirect_key'   => 'redirect',
 
     //控制模型数据列表页默认的显示条目
-    'global_rows_per_page' => 20,
+        'global_rows_per_page' => 20,
 
     //可选的语言，如果不为空，将会在页面顶部显示『选择语言』按钮
-    'locales' => [],
+        'locales'              => [],
 
-    'custom_routes_file' => app_path('Http/routes/administrator.php'),
-);
+        'custom_routes_file' => app_path('Http/routes/administrator.php'),
+];
